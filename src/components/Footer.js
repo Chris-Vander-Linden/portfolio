7f73c3d66b886f8ceb3e4df99ccf0913bf2daf1a
Chris Vander Linden <cvl123abc@gmail.com>
2023-01-21 20:36:26 -0600

    Remove second lang option from site

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,9 @@ class Footer extends Component {
     if (this.props.sharedBasicInfo) {
       var networks = this.props.sharedBasicInfo.social.map(function (network) {
         return (
-          <span key={network.name} className="m-4">
-            <a href={network.url} target="_blank" rel="noopener noreferrer">
-              <i className={network.class}></i>
+          <span key={ network.name } className="m-4">
+            <a href={ network.url } target="_blank" rel="noopener noreferrer">
+              <i className={ network.class }></i>
             </a>
           </span>
         );
@@ -18,49 +18,17 @@ class Footer extends Component {
       <footer>
         <div className="container">
           <div className="row">
-            <div className="col social-links">{networks}</div>
+            <div className="col social-links">{ networks }</div>
             <div className="col">
-              <div 
-                onClick={() =>
-                  this.props.applyPickedLanguage(
-                    window.$primaryLanguage,
-                    window.$secondaryLanguageIconId
-                  )
-                }
-                style={{ display: "inline" }}
-              >
-                <span
-                  className="iconify language-icon mr-5"
-                  data-icon="twemoji-flag-for-flag-united-kingdom"
-                  data-inline="false"
-                  id={window.$primaryLanguageIconId}
-                ></span>
-              </div>
-              <div 
-                onClick={() =>
-                  this.props.applyPickedLanguage(
-                    window.$secondaryLanguage,
-                    window.$primaryLanguageIconId
-                  )
-                }
-                style={{ display: "inline" }}
-              >
-                <span
-                  className="iconify language-icon"
-                  data-icon="twemoji-flag-for-flag-poland"
-                  data-inline="false"
-                  id={window.$secondaryLanguageIconId}
-                ></span>
-              </div>
             </div>
             <div className="col">
               <div className="copyright py-4 text-center">
                 <div className="container">
                   <small>
-                    Copyright &copy;{" "}
-                    {this.props.sharedBasicInfo
+                    Copyright &copy;{ " " }
+                    { this.props.sharedBasicInfo
                       ? this.props.sharedBasicInfo.name
-                      : "???"}
+                      : "???" }
                   </small>
                 </div>
               </div>
