950ccf92e18737859cd39b882a96eb752fe58719
Chris Vander Linden <cvl123abc@gmail.com>
2023-01-24 12:26:34 -0600

    Update theme style

---
+++ src/components/More.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+
+class More extends Component {
+  render() {
+
+    return (
+      <section>
+        <div className="col-md-12">
+          <h1 style={ { color: "black" } }>
+          <span>
+            More
+          </span>       
+          </h1>
+          <div className="row center mx-auto mb-5">
+            <div className="col-md-8 center">
+              <div className="col-md-10">
+                <div className="card">
+                  <div className="card-header">
+                    <span
+                      className="iconify"
+                      data-icon="emojione:red-circle"
+                      data-inline="false"
+                    ></span>{" "}
+                    &nbsp;{" "}
+                    <span
+                      className="iconify"
+                      data-icon="twemoji:yellow-circle"
+                      data-inline="false"
+                    ></span>{" "}
+                    &nbsp;{" "}
+                    <span
+                      className="iconify"
+                      data-icon="twemoji:green-circle"
+                      data-inline="false"
+                    ></span>
+                  </div>
+                  <div
+                    className="card-body font-trebuchet text-justify ml-3 mr-3"
+                    style={{
+                      height: "auto",
+                      fontSize: "132%",
+                      lineHeight: "200%",
+                    }}
+                  >
+                    <br />
+                    <span className="wave">A new title</span>
+                    <br />
+                    <br />
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero repellat excepturi quo unde ut dicta sapiente voluptas repudiandae maxime asperiores, optio vel, voluptate alias nobis sint impedit est tenetur dolores minus necessitatibus ex. Ea, nam eos. Dignissimos ex, maiores quam fugit deserunt, dolores quisquam ipsa nemo excepturi assumenda veniam aperiam? Recusandae quod, labore nulla voluptate adipisci cum assumenda impedit in? Assumenda, quo sed nesciunt obcaecati optio, vel cumque, voluptatibus laborum quod sit laboriosam sapiente architecto totam asperiores iusto vitae natus ducimus ipsum excepturi delectus enim soluta magni. Unde error quam dignissimos, doloremque labore cum repudiandae accusamus dolor nam consectetur, architecto facere natus sit totam sint? Adipisci quasi reprehenderit delectus suscipit eligendi magnam! Labore error voluptatum delectus ut consequatur atque vero saepe eveniet quis recusandae quas provident tenetur, iure nam cumque similique laborum earum, dolores pariatur suscipit officiis animi soluta. Quod laborum magni dolorum quas ipsa asperiores consectetur assumenda facilis fugiat cupiditate maxime, odio dolore nemo voluptate? Cumque facere esse perspiciatis, eligendi libero doloribus in delectus inventore vitae, ex harum ducimus facilis quidem dignissimos possimus dolorum rem corrupti eum? Eum minus perspiciatis cupiditate recusandae, ullam deleniti deserunt totam reiciendis sapiente, delectus dolorem veritatis qui ratione omnis libero quos? Cum voluptates explicabo iure vel, porro rem harum alias. Porro reprehenderit tenetur error impedit? Consequatur, illum. Similique impedit, deserunt illo totam nostrum maiores. Libero atque tempora autem voluptatibus velit saepe sed architecto quaerat, et doloribus aperiam sit repellendus, tempore repellat, ea consequuntur eligendi harum? Facilis id rerum quae libero! Minima velit ut, blanditiis voluptatem nostrum nemo! Accusantium sed ipsam magnam! Dolorem illo voluptatum dolore molestiae dicta esse beatae officiis, non cumque, optio reiciendis totam obcaecati maxime quibusdam nostrum magni atque perferendis possimus ab. Non odio perspiciatis dignissimos doloremque voluptas, adipisci tenetur eligendi aut veniam deserunt fugit animi blanditiis voluptatibus magni doloribus eaque! Est.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default More;
